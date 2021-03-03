6103db824b0dc2e4f12f90a7366d5f158097efa1
kstb-fns <fns.kstb@gmail.com>
2021-03-03 17:07:33 +0700

    tampilkan data anggota

--- controller.js
+++ controller.js
@@ -6,3 +6,28 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Aplikasi KSTB REST API ku berjalan", res)
 };
+
+
+//Menampilkan data semua Anggota
+exports.tampilsemuaanggota = function(req,res){
+    connection.query('SELECT * FROM tbl_anggota', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    })
+};
+
+//Menampilkan filter Anggota Id 'id_anggota'
+exports.tampilanggotaperid = function(req,res){
+    let id = req.param.id;
+    connection.query('SELECT * FROM tbl_anggota WHERE id_anggota = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows, res);
+            }
+    });
+};
